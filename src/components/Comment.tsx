f32be598f771546dc7721d45410393e4bd7e4d8a
Francois <francoischpn@gmail.com>
2023-02-06 20:48:38 +0100

    refact

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,37 +1,52 @@
-import React, { useState } from "react";
+// import React, { useContext, useState } from "react";
+// import { commentAPI } from "../api/commentAPI";
+// import  UserContext  from "../contexts/userContext";
+// import ProjectContext from "../contexts/projectContext";
 
-interface Comment {
-  text: string;
-}
+// interface Comment {
+//   text: string;
+// }
 
 const CommentSection = () => {
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [newComment, setNewComment] = useState("");
-
-  const getAllComment = () => {};
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setComments([...comments, { text: newComment }]);
-    setNewComment("");
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-        <textarea
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-        />
-        <button type="submit">Ajouter un commentaire</button>
-      </form>
-    </div>
-  );
+  //   const [comments, setComments] = useState<Comment[]>([]);
+  //   const { user, setUser } = useContext(UserContext);
+  //   const [newComment, setNewComment] = useState("");
+  //   const [fileID, setFileID] = useState<Number>();
+  //   const getAllComment = () => {
+  //     const comments = await commentAPI.getAll();
+  //     setComments(comments)
+  //   };
+  //   const getFileIdByUser = () => {
+  //     const fileID = await commentAPI.getAll();
+  //     setFileID(fileID)
+  //   };
+  //   const handleSubmit = (e: React.FormEvent) => {
+  //     e.preventDefault();
+  //     const comments = {
+  //       fileId: fileID.id;
+  //       comment: newComment;
+  //       userId: user.id;
+  //     }
+  //     await commentAPI.create(newComment)
+  //     setComments([...comments, { text: newComment }]);
+  //     setNewComment("");
+  //   };
+  //   return (
+  //     <div>
+  //       <form onSubmit={handleSubmit}>
+  //         <ul>
+  //           {comments.map((comment, index) => (
+  //             <li key={index}>{comment.text}</li>
+  //           ))}
+  //         </ul>
+  //         <textarea
+  //           value={newComment}
+  //           onChange={(e) => setNewComment(e.target.value)}
+  //         />
+  //         <button type="submit">Ajouter un commentaire</button>
+  //       </form>
+  //     </div>
+  //   );
 };
 
 export default CommentSection;
